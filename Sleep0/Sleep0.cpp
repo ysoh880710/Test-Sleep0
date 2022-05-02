f74debb03a23200cf81af133beea557c20aa8986
ysoh880710 <ysoh880710@gmail.com>
2022-05-02 19:05:05 +0900

    Fix False Sharing Problem

--- Sleep0/Sleep0.cpp
+++ Sleep0/Sleep0.cpp
@@ -3,12 +3,22 @@
 
 using namespace std;
 
-void* g_pThreads[4]{};
-unsigned long g_pThreadIDs[4]{};
+void* g_pThreads[8]{};
+unsigned long g_pThreadIDs[8]{};
 
-long long g_pExitThreads[4]{};
+long long g_pExitThreads0 = 0ll;
+long long g_pPadding0[8]{};
 
-unsigned long __stdcall Func(void* const _pParameter)
+long long g_pExitThreads1 = 0ll;
+long long g_pPadding1[8]{};
+
+long long g_pExitThreads2 = 0ll;
+long long g_pPadding2[8]{};
+
+long long g_pExitThreads3 = 0ll;
+long long g_pPadding[8]{};
+
+unsigned long __stdcall Func0(void* const _pParameter)
 {
     const unsigned long long ullThreadIndex = reinterpret_cast<unsigned long long>(_pParameter);
     const unsigned long long ullThreadStartTick = GetTickCount64();
@@ -19,7 +29,7 @@ unsigned long __stdcall Func(void* const _pParameter)
     
     while (true)
     {
-        if (_InterlockedCompareExchange64(&g_pExitThreads[ullThreadIndex],
+        if (_InterlockedCompareExchange64(&g_pExitThreads0,
                                           0ll,
                                           1ll) != 0ll)
         {
@@ -43,43 +53,175 @@ unsigned long __stdcall Func(void* const _pParameter)
     return 0ul;
 }
 
+unsigned long __stdcall Func1(void* const _pParameter)
+{
+    const unsigned long long ullThreadIndex = reinterpret_cast<unsigned long long>(_pParameter);
+    const unsigned long long ullThreadStartTick = GetTickCount64();
+
+    wcout << L"Thread[" << ullThreadIndex << L"]\tStartTick\t:\t" << ullThreadStartTick << endl;
+
+    unsigned long long ullThreadSpinCount = 0ull;
+
+    while (true)
+    {
+        if (_InterlockedCompareExchange64(&g_pExitThreads1,
+                                          0ll,
+                                          1ll) != 0ll)
+        {
+            break;
+        }
+
+        ++ullThreadSpinCount;
+
+        if (ullThreadIndex != 0ull)
+        {
+            Sleep(0ul);
+        }
+    }
+
+    const unsigned long long ullThreadEndTick = GetTickCount64();
+
+    wcout << L"Thread[" << ullThreadIndex << L"]\tEndTick\t:\t" << ullThreadEndTick << endl;
+    wcout << L"Thread[" << ullThreadIndex << L"]\tDuration\t:\t" << ullThreadEndTick - ullThreadStartTick << endl;
+    wcout << L"Thread[" << ullThreadIndex << L"]\tSpinCount\t:\t" << ullThreadSpinCount << endl;
+
+    return 0ul;
+}
+
+unsigned long __stdcall Func2(void* const _pParameter)
+{
+    const unsigned long long ullThreadIndex = reinterpret_cast<unsigned long long>(_pParameter);
+    const unsigned long long ullThreadStartTick = GetTickCount64();
+
+    wcout << L"Thread[" << ullThreadIndex << L"]\tStartTick\t:\t" << ullThreadStartTick << endl;
+
+    unsigned long long ullThreadSpinCount = 0ull;
+
+    while (true)
+    {
+        if (_InterlockedCompareExchange64(&g_pExitThreads2,
+                                          0ll,
+                                          1ll) != 0ll)
+        {
+            break;
+        }
+
+        ++ullThreadSpinCount;
+
+        if (ullThreadIndex != 0ull)
+        {
+            Sleep(0ul);
+        }
+    }
+
+    const unsigned long long ullThreadEndTick = GetTickCount64();
+
+    wcout << L"Thread[" << ullThreadIndex << L"]\tEndTick\t:\t" << ullThreadEndTick << endl;
+    wcout << L"Thread[" << ullThreadIndex << L"]\tDuration\t:\t" << ullThreadEndTick - ullThreadStartTick << endl;
+    wcout << L"Thread[" << ullThreadIndex << L"]\tSpinCount\t:\t" << ullThreadSpinCount << endl;
+
+    return 0ul;
+}
+
+unsigned long __stdcall Func3(void* const _pParameter)
+{
+    const unsigned long long ullThreadIndex = reinterpret_cast<unsigned long long>(_pParameter);
+    const unsigned long long ullThreadStartTick = GetTickCount64();
+
+    wcout << L"Thread[" << ullThreadIndex << L"]\tStartTick\t:\t" << ullThreadStartTick << endl;
+
+    unsigned long long ullThreadSpinCount = 0ull;
+
+    while (true)
+    {
+        if (_InterlockedCompareExchange64(&g_pExitThreads3,
+                                          0ll,
+                                          1ll) != 0ll)
+        {
+            break;
+        }
+
+        ++ullThreadSpinCount;
+
+        if (ullThreadIndex != 0ull)
+        {
+            Sleep(0ul);
+        }
+    }
+
+    const unsigned long long ullThreadEndTick = GetTickCount64();
+
+    wcout << L"Thread[" << ullThreadIndex << L"]\tEndTick\t:\t" << ullThreadEndTick << endl;
+    wcout << L"Thread[" << ullThreadIndex << L"]\tDuration\t:\t" << ullThreadEndTick - ullThreadStartTick << endl;
+    wcout << L"Thread[" << ullThreadIndex << L"]\tSpinCount\t:\t" << ullThreadSpinCount << endl;
+
+    return 0ul;
+}
+
 int main()
 {
     g_pThreads[0] = CreateThread(nullptr,
                                  0ull,
-                                 Func,
+                                 Func0,
                                  reinterpret_cast<void*>(0),
                                  0ul,
                                  &g_pThreadIDs[0]);
 
     g_pThreads[1] = CreateThread(nullptr,
                                  0ull,
-                                 Func,
+                                 Func1,
                                  reinterpret_cast<void*>(1),
                                  0ul,
                                  &g_pThreadIDs[1]);
 
     g_pThreads[2] = CreateThread(nullptr,
                                  0ull,
-                                 Func,
+                                 Func2,
                                  reinterpret_cast<void*>(2),
                                  0ul,
                                  &g_pThreadIDs[2]);
-
+    
     g_pThreads[3] = CreateThread(nullptr,
                                  0ull,
-                                 Func,
+                                 Func3,
                                  reinterpret_cast<void*>(3),
                                  0ul,
                                  &g_pThreadIDs[3]);
 
+    //g_pThreads[4] = CreateThread(nullptr,
+    //                             0ull,
+    //                             Func,
+    //                             reinterpret_cast<void*>(4),
+    //                             0ul,
+    //                             &g_pThreadIDs[4]);
+    //
+    //g_pThreads[5] = CreateThread(nullptr,
+    //                             0ull,
+    //                             Func,
+    //                             reinterpret_cast<void*>(5),
+    //                             0ul,
+    //                             &g_pThreadIDs[5]);
+    //
+    //g_pThreads[6] = CreateThread(nullptr,
+    //                             0ull,
+    //                             Func,
+    //                             reinterpret_cast<void*>(6),
+    //                             0ul,
+    //                             &g_pThreadIDs[6]);
+    //
+    //g_pThreads[7] = CreateThread(nullptr,
+    //                             0ull,
+    //                             Func,
+    //                             reinterpret_cast<void*>(7),
+    //                             0ul,
+    //                             &g_pThreadIDs[7]);
+
     Sleep(1000ul);
 
-    for (unsigned long long i = 0ull; i < 4ull; ++i)
-    {
-        _InterlockedExchange64(&g_pExitThreads[i],
-                               1ll);
-    }
+    _InterlockedExchange64(&g_pExitThreads0, 1ll);
+    _InterlockedExchange64(&g_pExitThreads1, 1ll);
+    _InterlockedExchange64(&g_pExitThreads2, 1ll);
+    _InterlockedExchange64(&g_pExitThreads3, 1ll);
 
     unsigned long ulExitCode = 10ul;
     while (true)
@@ -108,7 +250,7 @@ int main()
             break;
         }
     }
-
+    
     while (true)
     {
         GetExitCodeThread(g_pThreads[3], &ulExitCode);
@@ -117,6 +259,42 @@ int main()
             break;
         }
     }
+
+    //while (true)
+    //{
+    //    GetExitCodeThread(g_pThreads[4], &ulExitCode);
+    //    if (ulExitCode == 0ul)
+    //    {
+    //        break;
+    //    }
+    //}
+    //
+    //while (true)
+    //{
+    //    GetExitCodeThread(g_pThreads[5], &ulExitCode);
+    //    if (ulExitCode == 0ul)
+    //    {
+    //        break;
+    //    }
+    //}
+    //
+    //while (true)
+    //{
+    //    GetExitCodeThread(g_pThreads[6], &ulExitCode);
+    //    if (ulExitCode == 0ul)
+    //    {
+    //        break;
+    //    }
+    //}
+    //
+    //while (true)
+    //{
+    //    GetExitCodeThread(g_pThreads[7], &ulExitCode);
+    //    if (ulExitCode == 0ul)
+    //    {
+    //        break;
+    //    }
+    //}
 
     return 0;
 }
